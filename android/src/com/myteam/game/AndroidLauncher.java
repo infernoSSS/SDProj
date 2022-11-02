ec6b4849d2c4c608f8e7dec0c08196d0aeb1c5db
Курганников Андрей Алексеевич <kurgannikov.andrey@bk.ru>
2022-11-03 00:04:52 +0300

    Initial commit

---
+++ android/src/com/myteam/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.myteam.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.myteam.game.SDGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SDGame(), config);
+	}
+}
