8502f4af1320ef9a3ed24177be451e6f309d4143
Igrosmurf <prudkoydr@mail.ru>
2022-11-09 23:48:37 +0300

    Это база

--- desktop/src/com/myteam/game/DesktopLauncher.java
+++ desktop/src/com/myteam/game/DesktopLauncher.java
@@ -10,7 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Space and Dice");
-		config.setWindowedMode(720, 1240);
+		config.setWindowedMode(1240, 720);
 		new Lwjgl3Application(new SDGame(), config);
 	}
 }
